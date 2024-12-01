ce829248f51443ff0f925817c74b14a242bf154a
Nalin-kumar-gupta <nalingupta71@gmail.com>
2024-12-01 14:51:06 +0530

    appl v2

--- amd_core/settings.py
+++ amd_core/settings.py
@@ -12,21 +12,33 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from pathlib import Path
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+import environ
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '62qi_zv=2rpwyv6lr6724l+&lb5g*68lb-0^lh8srb+m@hbhbl'
+# SECRET_KEY = 'django-insecure-eya8qcjbbdjf!xdlzm_y3ap^zx9l-hejj1@q0l6zkpgb7p%9cj'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+env = environ.Env(DEBUG=(bool, False))
 
-ALLOWED_HOSTS = []
 
+environ.Env.read_env(os.path.join(BASE_DIR, ".env/.local/.django"))
+
+DEBUG = env("DEBUG")
+
+SECRET_KEY = env("SECRET_KEY")
+
+
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS")
+
+REDIS_URL = env("REDIS_URL")
 
 # Application definition
 
@@ -39,6 +51,10 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'rest_framework',
     'amd_api',
+    "corsheaders",
+    "django_celery_results",
+    "django_celery_beat",
+    "rest_framework_simplejwt",
 )
 
 MIDDLEWARE = [
@@ -76,29 +92,39 @@ WSGI_APPLICATION = 'amd_core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
+ELASTICSEARCH_DSL = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'hosts': 'http://elasticsearch:9200'  # Update with your Elasticsearch host/port
+    },
 }
 
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
-USE_L10N = True
-
 USE_TZ = True
 
+WSGI_APPLICATION = "amd_core.wsgi.application"
+
+CELERY_BROKER_URL = env("BROKER_URL")
+CELERY_RESULT_BACKEND = env("CELERY_RESULT_BACKEND")
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.8/howto/static-files/
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
+STATIC_ROOT = "/vol/static/"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = "/vol/media/"
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
